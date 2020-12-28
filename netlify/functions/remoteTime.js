9493a470ae785ac0dd1af265068b32b4561704d3
mijorus <lorenzo.mijorus@gmail.com>
2020-12-28 17:19:04 +0100

    only accept get requests

--- netlify/functions/remoteTime.js
+++ netlify/functions/remoteTime.js
@@ -3,28 +3,36 @@ const axios = require('axios');
 exports.handler = async function (event, context) {
     const ip = event.headers['client-ip'];
 
-    try {
-        const res = await axios.get('https://ipapi.co/' + 'ip' + '/json/', {
-            headers: {
-                'content-type': 'application/json'
-            }
-        })
+console.log(event)
+    if (event.httpMethod === 'GET') {
+        try {
+            const res = await axios.get('https://ipapi.co/' + ip + '/json/', {
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
 
-        if (!res.data.error) {
+            if (!res.data.error) {
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({
+                        unixtime: Math.floor(Date.now() / 1000),
+                        timezone: res.data.timezone,
+                    })
+                }
+            } else { throw 'Request Errors' }
+
+
+        } catch (err) {
             return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    unixtime: Math.floor(Date.now() / 1000),
-                    timezone: res.data.timezone,
-                })
+                statusCode: 500,
+                body: 'Request failed'
             }
-        } else { throw 'Request Errors' }
-
-        
-    } catch (err) {
+        }
+    } else {
         return {
-            statusCode: 500,
-            body: 'Request failed'
+            statusCode: 501,
+            body: 'Method not supported'
         }
     }
 }
