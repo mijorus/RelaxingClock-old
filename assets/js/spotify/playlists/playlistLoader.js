9f73a7b6ec66cdc0fe7780e4fd16d56ead3df369
mijorus <lorenzo.mijorus@gmail.com>
2021-01-21 18:22:03 +0100

    display default playlist

--- assets/js/spotify/playlists/playlistLoader.js
+++ assets/js/spotify/playlists/playlistLoader.js
@@ -26,9 +26,9 @@ export function playlistLoader(container) {
     listContainer = container;
     if (userLibrary.length === 0 || defaultLibrary.length === 0) {
         $.when(getPlaylistList(), getShowList(), getPlaylistData(defaultPlaylistsArray[0]), getPlaylistData(defaultPlaylistsArray[1]))
-            .done((playlists, shows, default1) => {
+            .done((playlists, shows, default1, default2) => {
                 userLibrary.push(playlists[0].items, shows[0].items);
-                defaultLibrary.push(default1[0]);
+                defaultLibrary.push(default1[0], default2[0]);
                 loadUserList(userLibrary);
             })
     } else {
