326b7f78ebb0e1d744244f8d15599b40e5a405cd
mijorus <lorenzo.mijorus@gmail.com>
2021-02-13 10:42:42 +0100

    refactoring

--- server/functions/remoteTime.js
+++ server/functions/remoteTime.js
@@ -4,35 +4,36 @@ exports.handler = async function (event, context) {
     const ip = event.headers['client-ip'];
 
 console.log(event)
-    if (event.httpMethod === 'GET') {
-        try {
-            const res = await axios.get('https://ipapi.co/' + ip + '/json/', {
-                headers: {
-                    'content-type': 'application/json'
-                }
-            })
-
-            if (!res.data.error) {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({
-                        unixtime: Math.floor(Date.now() / 1000),
-                        timezone: res.data.timezone,
-                    })
-                }
-            } else { throw 'Request Errors' }
-
-
-        } catch (err) {
-            return {
-                statusCode: 500,
-                body: 'Request failed'
-            }
-        }
-    } else {
+    if (event.httpMethod !== 'GET') {
         return {
             statusCode: 405,
             body: 'Method not supported'
+        }
+    }
+
+    try {
+        const res = await axios.get('https://ipapi.co/' + ip + '/json/', {
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+
+        if (res.data.error) {
+            throw 'Request Errors';
+        } else { 
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    unixtime: Math.floor(Date.now() / 1000),
+                    timezone: res.data.timezone,
+                })
+            }
+        }
+
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: 'Request failed'
         }
     }
 }
